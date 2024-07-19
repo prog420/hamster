1f459dc4103aa83c9d85a9cdbf999455d460ed92
Dmitry B.V <83430886+prog420@users.noreply.github.com>
2024-07-19 21:50:13 +0500

    Add sync requests

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ from env import Env
 
 BASE_URL = "https://api.hamsterkombatgame.io"
 TAP_URL = f"{BASE_URL}/clicker/tap"
+ACC_INFO_URL = f"{BASE_URL}/auth/account-info"
+SYNC_URL = f"{BASE_URL}/clicker/sync"
+CONFIG_URL = f"{BASE_URL}/clicker/config"
+
 BEARER_TOKEN = f"Bearer {Env.BEARER_TOKEN}"
 
 headers = {"Authorization": BEARER_TOKEN, "Content-Type": "application/json"}
+
+requests.post(url=ACC_INFO_URL, headers=headers)
+time.sleep(1)
+requests.post(url=SYNC_URL, headers=headers)
+time.sleep(1)
+requests.post(url=CONFIG_URL, headers=headers)
 
 # Set tap count to 1 and send request to check available taps.
 payload = {"count": 1, "availableTaps": 9000, "timestamp": 1721320096}
